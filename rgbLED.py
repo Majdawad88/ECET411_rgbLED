da7b975d93b02b7fb67a9ee4b554b234572f1f3a
Majdawad88 <47667880+Majdawad88@users.noreply.github.com>
2025-01-16 19:56:58 -0500

    Add files via upload

---
+++ rgbLED.py
@@ -0,0 +1,81 @@
+  GNU nano 7.2                                                                     rgb.py
+#libraries
+import RPi.GPIO as GPIO
+from time import sleep
+#disable warnings (optional)
+GPIO.setwarnings(False)
+#Select GPIO Mode
+GPIO.setmode(GPIO.BCM)
+#set red,green and blue pins
+redPin = 12
+greenPin = 19
+bluePin = 13
+#set pins as outputs
+GPIO.setup(redPin,GPIO.OUT)
+GPIO.setup(greenPin,GPIO.OUT)
+GPIO.setup(bluePin,GPIO.OUT)
+
+def turnOff():
+        GPIO.output(redPin,GPIO.LOW)
+        GPIO.output(greenPin,GPIO.LOW)
+        GPIO.output(bluePin,GPIO.LOW)
+        print("off")
+
+def white():
+        GPIO.output(redPin,GPIO.HIGH)
+        GPIO.output(greenPin,GPIO.HIGH)
+        GPIO.output(bluePin,GPIO.HIGH)
+        print("white")
+
+def red():
+        GPIO.output(redPin,GPIO.HIGH)
+        GPIO.output(greenPin,GPIO.LOW)
+        GPIO.output(bluePin,GPIO.LOW)
+        print("red")
+
+def green():
+        GPIO.output(redPin,GPIO.LOW)
+        GPIO.output(greenPin,GPIO.HIGH)
+        GPIO.output(bluePin,GPIO.LOW)
+        print("green")
+def blue():
+        GPIO.output(redPin,GPIO.LOW)
+        GPIO.output(greenPin,GPIO.LOW)
+        GPIO.output(bluePin,GPIO.HIGH)
+        print("blue")
+
+def yellow():
+        GPIO.output(redPin,GPIO.HIGH)
+        GPIO.output(greenPin,GPIO.HIGH)
+        GPIO.output(bluePin,GPIO.LOW)
+        print("yellow")
+
+def purple():
+        GPIO.output(redPin,GPIO.HIGH)
+        GPIO.output(greenPin,GPIO.LOW)
+        GPIO.output(bluePin,GPIO.HIGH)
+        print("purple")
+
+def lightBlue():
+        GPIO.output(redPin,GPIO.LOW)
+        GPIO.output(greenPin,GPIO.HIGH)
+        GPIO.output(bluePin,GPIO.HIGH)
+        print("light blue")
+
+while True:
+        turnOff()
+        sleep(5) #1second
+        white()
+        sleep(5)
+        red()
+        sleep(5)
+        green()
+        sleep(5)
+        blue()
+        sleep(5)
+        yellow()
+        sleep(5)
+        purple()
+        sleep(5)
+        lightBlue()
+        sleep(5)
